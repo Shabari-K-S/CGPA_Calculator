e92ed2998deaeabea42ce90c212b18e3823682cd
Shabari-K-S <shabaricse2003@gmail.com>
2023-09-08 22:34:06 +0530

    Fixed

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const toc = document.getElementsByClassName("toc")[0];
+const aiml = document.getElementsByClassName("aiml").value;
+const ncc = document.getElementsByClassName("ncc").value;
+const dbms = document.getElementsByClassName("dbms").value;
+
+const btn = document.getElementsByClassName("btn").value;
+
+btn.addEventListener("click", clicked());
+function clicked(){
+    console.log("clicked");
+    console.log(toc.value);
+}
