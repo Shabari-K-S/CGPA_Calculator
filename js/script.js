3960f4e749935d87436ef43cc68ded91dbb3158c
Shabari-K-S <shabaricse2003@gmail.com>
2023-09-08 23:20:09 +0530

    Final

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-const toc = document.getElementsByClassName("toc").value;
-const aiml = document.getElementsByClassName("aiml").value;
-const ncc = document.getElementsByClassName("ncc").value;
-const dbms = document.getElementsByClassName("dbms").value;
+const tocInput = document.querySelector(".toc");
+const aimlInput = document.querySelector(".aiml");
+const nccInput = document.querySelector(".ncc");
+const dbmsInput = document.querySelector(".dbms");
+const algInput = document.querySelector(".alg");
+const osInput = document.querySelector(".os");
+const essInput = document.querySelector(".ess");
+const oslInput = document.querySelector(".osl");
+const dbmslInput = document.querySelector(".dbmsl");
 
-const btn = document.getElementsByClassName("btn");
+const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", function clicked() {
-  console.log("clicked");
-  console.log(toc.value);
+btn.addEventListener("click", function() {
+    const toc = parseFloat(tocInput.value);
+    const aiml = parseFloat(aimlInput.value);
+    const ncc = parseFloat(nccInput.value);
+    const dbms = parseFloat(dbmsInput.value);
+    const alg = parseFloat(algInput.value);
+    const os = parseFloat(osInput.value);
+    const ess = parseFloat(essInput.value);
+    const osl = parseFloat(oslInput.value);
+    const dbmsl = parseFloat(dbmslInput.value);
+
+    if (!isNaN(toc) && !isNaN(aiml) && !isNaN(ncc) && !isNaN(dbms) && !isNaN(alg) && !isNaN(os) && !isNaN(ess) && !isNaN(osl) && !isNaN(dbmsl)) {
+        const totalCredits = 9; 
+        const cgpa = (toc + aiml + ncc + dbms + alg + os + ess + osl + dbmsl) / totalCredits;
+
+        const display = document.querySelector(".display");
+        display.textContent = "GPA :" + cgpa.toFixed(2);
+    } else {
+        alert("Please enter valid inputs");
+    }
 });
